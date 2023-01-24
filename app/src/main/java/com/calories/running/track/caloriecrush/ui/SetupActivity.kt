650e0b70ce595df7ef62942db009bd3e20add9e3
yash30401 <yashveersingh30401@gmail.com>
2023-01-24 18:40:50 +0530

    1.Setting Up SetupActivity.kt

--- app/src/main/java/com/calories/running/track/caloriecrush/ui/SetupActivity.kt
+++ app/src/main/java/com/calories/running/track/caloriecrush/ui/SetupActivity.kt
@@ -3,18 +3,16 @@ package com.calories.running.track.caloriecrush.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.calories.running.track.caloriecrush.R
-import com.calories.running.track.caloriecrush.databinding.ActivityMainBinding
+import com.calories.running.track.caloriecrush.databinding.ActivitySetupBinding
 
+class SetupActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySetupBinding
 
-    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding= ActivitySetupBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
 
 
     }
