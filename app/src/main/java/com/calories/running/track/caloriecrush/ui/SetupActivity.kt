2ab0d825f2abbde9ae868cbf881ac354ccaf3dc0
yash30401 <yashveersingh30401@gmail.com>
2023-01-24 20:43:29 +0530

    1. animtion part Done

--- app/src/main/java/com/calories/running/track/caloriecrush/ui/SetupActivity.kt
+++ app/src/main/java/com/calories/running/track/caloriecrush/ui/SetupActivity.kt
@@ -2,6 +2,7 @@ package com.calories.running.track.caloriecrush.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import com.calories.running.track.caloriecrush.R
@@ -13,6 +14,8 @@ class SetupActivity : AppCompatActivity() {
     private lateinit var slideUp: Animation
     private lateinit var fadeIn:Animation
     private lateinit var slideSlow:Animation
+    private lateinit var slideLeft:Animation
+    private lateinit var slideUpAndFadeIn:Animation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivitySetupBinding.inflate(layoutInflater)
@@ -21,8 +24,19 @@ class SetupActivity : AppCompatActivity() {
         slideUp = AnimationUtils.loadAnimation(this,R.anim.slide_up)
         fadeIn = AnimationUtils.loadAnimation(this,R.anim.fade_in)
         slideSlow=AnimationUtils.loadAnimation(this,R.anim.slide_up_slow)
+        slideLeft=AnimationUtils.loadAnimation(this,R.anim.slide_left)
+        slideUpAndFadeIn=AnimationUtils.loadAnimation(this,R.anim.slide_up_and_fadein)
 
         startAnimation()
+
+        binding.enterWeightBtn.setOnClickListener {
+            binding.kgText.visibility= View.VISIBLE
+            binding.numberPicker.visibility=View.VISIBLE
+
+            binding.kgText.animation=slideUpAndFadeIn
+            binding.numberPicker.animation=slideUpAndFadeIn
+        }
+
     }
 
     private fun startAnimation() {
@@ -30,5 +44,6 @@ class SetupActivity : AppCompatActivity() {
         binding.imageView.animation=fadeIn
         binding.imageView.animation=fadeIn
         binding.enterWeightBtn.animation=slideSlow
+        binding.continueBtn.animation=slideLeft
     }
 }
