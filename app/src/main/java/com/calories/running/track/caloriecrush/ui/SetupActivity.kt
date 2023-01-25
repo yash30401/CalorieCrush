8d29328204e0a9a5269c79d79f006f7b2d5048f4
Yashveer singh <yashveersingh30401@gmail.com>
2023-01-25 19:54:42 +0530

    1. Validation Added

--- app/src/main/java/com/calories/running/track/caloriecrush/ui/SetupActivity.kt
+++ app/src/main/java/com/calories/running/track/caloriecrush/ui/SetupActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import com.calories.running.track.caloriecrush.R
 import com.calories.running.track.caloriecrush.databinding.ActivitySetupBinding
 
@@ -33,8 +34,8 @@ class SetupActivity : AppCompatActivity() {
         binding.enterWeightBtn.setOnClickListener {
 
             binding.numberPicker.visibility=View.VISIBLE
-
-
+            binding.chipGroup.visibility=View.VISIBLE
+            binding.chipGroup.animation=slideUpAndFadeIn
             binding.numberPicker.animation=slideUpAndFadeIn
         }
 
@@ -79,6 +80,21 @@ class SetupActivity : AppCompatActivity() {
 
         }
 
+        binding.continueBtn.setOnClickListener {
+            checkValidation()
+        }
+
+    }
+
+    private fun checkValidation() {
+        val name=binding.etName.editText?.text.toString()
+        if(name.isEmpty()){
+            Toast.makeText(this, "Please Enter Your Name", Toast.LENGTH_SHORT).show()
+        }else if(binding.chipGroup.visibility==View.GONE){
+            Toast.makeText(this, "Please Enter Your Weight", Toast.LENGTH_SHORT).show()
+        }else if(binding.kgChip.isChecked==false && binding.poundsChip.isChecked==false){
+            Toast.makeText(this, "Please Select Your Weight Units", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun startAnimation() {
