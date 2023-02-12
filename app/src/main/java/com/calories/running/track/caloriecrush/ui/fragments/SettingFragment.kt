9aba5cb9adffd4f96399392619d209c023ac3f47
Yashveer singh <yashveersingh30401@gmail.com>
2023-02-12 13:52:35 +0530

    1. Setting Screen Pref Putting Done

--- app/src/main/java/com/calories/running/track/caloriecrush/ui/fragments/SettingFragment.kt
+++ app/src/main/java/com/calories/running/track/caloriecrush/ui/fragments/SettingFragment.kt
@@ -12,7 +12,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.content.edit
 import com.calories.running.track.caloriecrush.R
 import com.calories.running.track.caloriecrush.databinding.FragmentSettingBinding
 
@@ -29,30 +31,60 @@ class SettingFragment : Fragment(R.layout.fragment_setting) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSettingBinding.bind(view)
 
+        sharerdPreferences = context?.getSharedPreferences("PREF", Context.MODE_PRIVATE)
         setupSettingScreen()
 
         binding.cvName.setOnClickListener {
 
             binding.etName.focusable = View.FOCUSABLE
-            binding.etName.isFocusableInTouchMode=true
+            binding.etName.isFocusableInTouchMode = true
             binding.etName.requestFocus()
-           val manager:InputMethodManager= activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            manager.showSoftInput(binding.etName,0)
+            val manager: InputMethodManager =
+                activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            manager.showSoftInput(binding.etName, 0)
         }
 
         binding.cvWeight.setOnClickListener {
 
             binding.etWeight.focusable = View.FOCUSABLE
-            binding.etWeight.isFocusableInTouchMode=true
+            binding.etWeight.isFocusableInTouchMode = true
             binding.etWeight.requestFocus()
-            val manager:InputMethodManager= activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            manager.showSoftInput(binding.etWeight,0)
+            val manager: InputMethodManager =
+                activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            manager.showSoftInput(binding.etWeight, 0)
+        }
+
+        binding.btnApplyChanges.setOnClickListener {
+            applyChangesToPrefs()
         }
 
     }
 
+    private fun applyChangesToPrefs() {
+        val weightUnit = sharerdPreferences?.getString("weight_unit", "-1")
+
+
+        if (weightUnit == "Kg") {
+
+            sharerdPreferences?.edit {
+                this.putString("user_name", binding.etName.text.toString()).apply()
+                this.putFloat("weight", binding.etWeight.text.toString().toFloat()).apply()
+            }
+
+        } else {
+            val weightInPounds = binding.etWeight.text.toString().toFloat()/2.20462f
+            sharerdPreferences?.edit {
+                this.putString("user_name", binding.etName.text.toString()).apply()
+                this.putFloat("weight",weightInPounds).apply()
+            }
+        }
+        binding.etName.clearFocus()
+        binding.etWeight.clearFocus()
+        Toast.makeText(context, "Changes Saved", Toast.LENGTH_SHORT).show()
+    }
+
     private fun setupSettingScreen() {
-        sharerdPreferences = context?.getSharedPreferences("PREF", Context.MODE_PRIVATE)
+
 
         val weightUnit = sharerdPreferences?.getString("weight_unit", "-1")
         var weight = sharerdPreferences?.getFloat("weight", 0f)?.toFloat()
