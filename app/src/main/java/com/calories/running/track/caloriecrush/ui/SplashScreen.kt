716fb70f68d075834c2badae8a25d1e636478467
Yashveer singh <yashveersingh30401@gmail.com>
2023-02-17 15:54:36 +0530

    1. Ui Completed Of a SplashScreen.kt

--- app/src/main/java/com/calories/running/track/caloriecrush/ui/SplashScreen.kt
+++ app/src/main/java/com/calories/running/track/caloriecrush/ui/SplashScreen.kt
@@ -2,10 +2,17 @@ package com.calories.running.track.caloriecrush.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.calories.running.track.caloriecrush.R
 
 class SplashScreen : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+
+
     }
 }
